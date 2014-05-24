569e1e59f2541244559db915f6999d79dc4a715d
glic3 <glic3@XPS13.(none)>
2014-05-24 13:14:41 +0200

    traceroute

--- traceroute.sh
+++ traceroute.sh
@@ -1,10 +1,13 @@
-#cat nodes.list | while read IP; do scp -o stricthostkeychecking=no traceroute.sh root@[$IP]: &; done
-#cat nodes.list | bash execute.sh "bash traceroute.sh" > hops
+#cat nodes.list | while read IP; do scp -o stricthostkeychecking=no traceroute.sh root@[$IP]: & done
+#cat nodes.list | bash execute.sh "bash traceroute.sh" > results/hops
 
 
 IPS="10.139.40.123 "$(cat bootstrap_unstable|awk {'print $1'})
+HOPS=""
 
 for IP in $(echo -n "$IPS"); do
-    ping -c 1 $IP -w 7 &> /dev/null &&
-    traceroute -w 1 -n $IP | tail -n1 | awk {'print $1'} 2> /dev/null
+    ping -c 1 $IP -w 3 &> /dev/null &&
+    HOPS="$HOPS $(traceroute -w 1 -n $IP | tail -n1 | awk {'print $1'} 2> /dev/null)"
 done
+
+echo "$(hostname) $(echo $HOPS | tr ' ' ',')"
