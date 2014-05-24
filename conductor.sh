4b072e81bbe4988c84021982fad695c5f41de058
glic3rinu <marcay@pangea.org>
2014-05-24 17:36:39 +0200

    d

--- conductor.sh
+++ conductor.sh
@@ -9,8 +9,10 @@ COUNTER=$(wc -l nodes.list|awk {'print $1'})
 #sleep 180
 #killall ssh
 
+#get_two_bucked_ids $COUNTER
+#exit
 
-mv /tmp/infohashes.list1 /tmp/infohashes.list
+cp /tmp/infohashes.list1 /tmp/infohashes.list
 python split-nodes.py | bash deploy.sh 4
 sleep $((62*60))
 python split-nodes.py | bash execute.sh "bash collect.sh" > results/exp4-cache100-nodes1
@@ -32,7 +34,7 @@ killall ssh
 
 sed -i 's/DEFAULT_NUM_NODES = 2\/" /DEFAULT_NUM_NODES = 1\/" /' experiment4.sh
 
-mv /tmp/infohashes.list2 /tmp/infohashes.list
+cp /tmp/infohashes.list2 /tmp/infohashes.list
 python split-nodes.py 3 0 | bash deploy.sh 4
 sleep $((20*60))
 python split-nodes.py 3 1 | bash deploy.sh 4
@@ -69,7 +71,7 @@ sed -i 's/sed -i "s\/if rtt/#sed -i "s\/if rtt/' experiment4.sh
 sed -i 's/sed -i "s\/^MAX_NUM_TIMEOUTS/#sed -i "s\/^MAX_NUM_TIMEOUTS/' experiment4.sh
 
 
-mv /tmp/infohashes.list3 /tmp/infohashes.list
+cp /tmp/infohashes.list3 /tmp/infohashes.list
 python split-nodes.py | bash deploy.sh 4
 sleep $((62*60))
 python split-nodes.py | bash execute.sh "bash collect.sh" > results/exp4-cache100-original-nodes2
@@ -78,7 +80,7 @@ python split-nodes.py | bash execute.sh "bash collect.sh" > results/exp4-cache10
 sleep 180
 killall ssh
 
-mv /tmp/infohashes.list4 /tmp/infohashes.list
+cp /tmp/infohashes.list4 /tmp/infohashes.list
 python split-nodes.py 3 0 | bash deploy.sh 4
 sleep $((20*60))
 python split-nodes.py 3 1 | bash deploy.sh 4
